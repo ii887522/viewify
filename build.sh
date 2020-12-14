a56f69d80eebe7735b2c1f7504cdd7f323af5f40
Chi Min Yong <chimin.yong@g2g.com>
2020-12-14 17:55:43 +0800

    Fix image names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker run --rm --name viewify_debug_x86_builder -w C:/viewify -v $PWD:C:/viewify ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Debug /p:Platform=x86
-docker run --rm --name viewify_debug_x64_builder -w C:/viewify -v $PWD:C:/viewify ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Debug /p:Platform=x64
-docker run --rm --name viewify_release_x86_builder -w C:/viewify -v $PWD:C:/viewify ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Release /p:Platform=x86
-docker run --rm --name viewify_release_x64_builder -w C:/viewify -v $PWD:C:/viewify ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Release /p:Platform=x64
+docker run --rm --name viewify_debug_x86_builder -w C:/viewify -v $PWD:C:/viewify viewify-ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Debug /p:Platform=x86
+docker run --rm --name viewify_debug_x64_builder -w C:/viewify -v $PWD:C:/viewify viewify-ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Debug /p:Platform=x64
+docker run --rm --name viewify_release_x86_builder -w C:/viewify -v $PWD:C:/viewify viewify-ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Release /p:Platform=x86
+docker run --rm --name viewify_release_x64_builder -w C:/viewify -v $PWD:C:/viewify viewify-ms-build-tools msbuild C:/viewify/viewify/viewify.sln /p:Configuration=Release /p:Platform=x64
--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-docker run --rm --name viewify_installer -w C:/viewify -v $PWD:C:/viewify custom-node ncu.cmd -u && npm.cmd install
+docker run --rm --name viewify_installer -w C:/viewify -v $PWD:C:/viewify viewify-custom-node ncu.cmd -u && npm.cmd install
