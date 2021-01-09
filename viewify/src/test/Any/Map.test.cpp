96758ccdc17f6cb9a4fbce5f5a10b2cc06c68d00
Chi Min Yong <chimin.yong@g2g.com>
2021-01-09 18:17:35 +0800

    Add Map

---
+++ viewify/src/test/Any/Map.test.cpp
@@ -0,0 +1,39 @@
+#ifdef TEST
+
+#include "Map.test.h"
+#include "../../main/Any/Map.h"
+#include "../../main/Struct/Size.h"
+#include "../../main/Struct/Point.h"
+#include <cassert>
+
+namespace ii887522::viewify
+{
+	static void testSet()
+	{
+		{
+			Map<unsigned int> map{ Size{ 3u, 3u } };
+			map.set(Point{ 0u, 0u }, 1u);
+			assert(map.get(Point{ 0u, 0u }) == 1u);
+			map.set(Point{ 1u, 1u }, 1u);
+			assert(map.get(Point{ 1u, 1u }) == 1u);
+			map.set(Point{ 1u, 1u }, 2u);
+			assert(map.get(Point{ 1u, 1u }) == 2u);
+		}
+		{
+			Map<unsigned int> map{ Size{ 5u, 5u } };
+			map.set(Point{ 0u, 0u }, 1u);
+			assert(map.get(Point{ 0u, 0u }) == 1u);
+			map.set(Point{ 1u, 1u }, 1u);
+			assert(map.get(Point{ 1u, 1u }) == 1u);
+			map.set(Point{ 1u, 1u }, 2u);
+			assert(map.get(Point{ 1u, 1u }) == 2u);
+		}
+	}
+
+	void testMap()
+	{
+		testSet();
+	}
+}
+
+#endif
