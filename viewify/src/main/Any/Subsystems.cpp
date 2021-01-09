117f705ed4b95111bceb5a3634a6f37b4ae35959
Chi Min Yong <chimin.yong@g2g.com>
2021-01-09 18:21:46 +0800

    Add subsystems

---
+++ viewify/src/main/Any/Subsystems.cpp
@@ -0,0 +1,25 @@
+#ifndef TEST
+
+#include "Subsystems.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+namespace ii887522::viewify
+{
+	Subsystems::Subsystems()
+	{
+		SDL_Init(SDL_INIT_VIDEO);
+		IMG_Init(IMG_INIT_PNG);
+		TTF_Init();
+	}
+
+	Subsystems::~Subsystems()
+	{
+		TTF_Quit();
+		IMG_Quit();
+		SDL_Quit();
+	}
+}
+
+#endif
