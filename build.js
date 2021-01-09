6a290d58b4b5eb401afd7f098e5100d5c2beaf1d
Chi Min Yong <chimin.yong@g2g.com>
2021-01-09 18:11:58 +0800

    Add App

--- build.js
+++ build.js
@@ -6,11 +6,14 @@ options.outDirPath = 'libs/'
 options.x86DllOutDirPaths = ['viewify/Debug/', 'viewify/Release/', 'viewify/Test/'],
 options.x64DllOutDirPaths = ['viewify/x64/Debug/', 'viewify/x64/Release/', 'viewify/x64/Test/']
 
+const accessToken = 'bt2pXeqCYPKNq5awCwN6'
+
 dependencies(async () => {
     await Promise.all([
         zip('https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip'),
         zip('https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip'),
-        zip('https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip')
+        zip('https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip'),
+        zip('https://gitlab.com/api/v4/projects/23530641/packages/generic/nitro/1.0.1/nitro-1.0.1.zip', { 'PRIVATE-TOKEN': accessToken })
     ])
     dll('x86', 'SDL2-2.0.12/lib/x86/SDL2.dll')
     dll('x64', 'SDL2-2.0.12/lib/X64/SDL2.dll')
